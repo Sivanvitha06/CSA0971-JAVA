e6d2ce41462d021b0975b225b7b826d54b46e474
Sivanvitha06 <122259215+Sivanvitha06@users.noreply.github.com>
2024-07-18 08:55:30 +0530

    Add files via upload

---
+++ ConstantExample.java
@@ -0,0 +1,9 @@
+class ConstantExample {
+    // Declaring a constant
+    public static final int MAX_VALUE = 100;
+
+    public static void main(String[] args) {
+        // Using the constant
+        System.out.println("The maximum value is: " + MAX_VALUE);
+    }
+}
---
+++ DivisionExample.java
@@ -0,0 +1,6 @@
+class  DivisionExample {
+    public static void main(String[] args) {
+        int result = 10 / 3;
+        System.out.println(result);
+    }
+}
